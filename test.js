5c1fd8c4d86f31fbe09483f77a1583de09d0aeff
Marynatest <marinamoskalets93799@gmail.com>
2022-09-18 22:40:39 +0300

    homework 2

---
+++ test.js
@@ -0,0 +1,16 @@
+alert ('test' + false); // "testfalse"
+alert ('one' + 5); // "one5"
+alert (true + 1); // 2
+
+alert ('6' * false); // 0
+alert ('4' * 4); // 16
+alert (8 * true); // 8
+
+alert ('test' / 6 ); // NaN
+alert ('6'/ true); // 6
+alert (8 / true); // 8 
+
+alert( Number("649") ); // 649 
+alert(String (false)); // "false" 
+alert( Boolean('Summer') ); // true
+alert( Boolean("") );  // false
